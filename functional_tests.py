9ec17fbf2410d2f6769489f505c5ec9343b627fa
i-mure <isaiahmureithi@yahoo.com>
2017-02-10 10:42:58 +0300

    Duplicated code smells

--- functional_tests.py
+++ functional_tests.py
@@ -35,8 +35,22 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows  = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
+
+		#Assert that the to-do item has been saved/ send
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])	
+	
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		# The page updates again, and now shows both items on her list
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn(
+			'2: Use peacock feathers to make a fly' ,
+			 [row.text for row in rows]
 		)
 
 		self.fail('Finish the test!')
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -5,5 +5,15 @@ class HomePageTest(TestCase):
 	"""Ensure the correct home page is being loaded"""
 
 	def test_home_page_returns_correct_html(self):
+
 		response = self.client.get("/")
 		self.assertTemplateUsed(response, 'home.html')
+
+
+	def test_can_save_a_POST_request(self):
+		
+		response = self.client.post("/", data={'item_text': 'A new list item'})
+		self.assertIn('A new list item', response.content.decode())
+		self.assertTemplateUsed(response, 'home.html')
+		
+
