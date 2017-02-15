8439eea55974f5f24a31b71c3ea4ae6040355768
i-mure <isaiahmureithi@yahoo.com>
2017-02-15 07:58:24 +0300

    Testing unique URL's for users

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -64,4 +64,45 @@ class NewVisitorTest(LiveServerTestCase):
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-		self.fail('Finish the test!')
+
+	def test_can_start_a_list_for_one_user(self):
+		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+
+	def test_multiple_users_can_start_lists_at_different_urls(self):
+		self.browser.get(self.live_server_url)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
+
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		# New user gets a totally new list
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy milk')
+
+		# New user gets own unique URL
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, edith_list_url)
+
+		# Ensure not trace of previous user's list
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('1: Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
+
+
+
